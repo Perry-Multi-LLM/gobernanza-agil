562d3a8d3c85cc53882412688c9a307b80f66276
Antonio <145431629+aperezmarquez@users.noreply.github.com>
2025-12-01 16:58:07 +0100

    Update: collect_issue_data.sh

--- scripts/collect_issue_data.sh
+++ scripts/collect_issue_data.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+mkdir -p metrics
+
+curl -H "Authorization: token $GITHUB_TOKEN" \
+     -H "Accept: application/vnd.github+json" \
+     https://api.github.com/repos/$GITHUB_REPOSITORY/issues?state=all \
+     > metrics/issues.json
